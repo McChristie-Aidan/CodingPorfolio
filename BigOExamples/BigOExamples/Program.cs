0cafecf2dbf229c203f6bc8f7e640c2716ba1445
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-09-19 19:38:50 -0500

    adds the BigO examples

---
+++ BigOExamples/BigOExamples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigOExamples
+{
+    class Program
+    {
+        List<string> line = new List<string>();
+        List<int> nums = new List<int>();
+        static void Main()
+        {
+        }
+
+        // O(1)
+        //this method is constant because no matter what you input it only has 1 action
+        private void PrintFirstInLine(List<string> line)
+        {
+            Console.WriteLine(line[0]);
+        }
+
+        // O(n) or linear
+        //this method is linear because the runtime gets longer of shorter depending on what you input for n
+        private void PopulateList(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums.Add(i+1);
+                Console.WriteLine(i+1);
+            }
+        }
+
+        // O(2^n)
+        //this method is quadratic because it will walk through the entire list a number of times equal to the number of items in the list
+        //therefore more its mean exponentially more time spent 
+        private void NumCompare(List<int> nums)
+        {
+            foreach (int firstNum in nums)
+            {
+                foreach (int secondNum in nums)
+                {
+                    int result = firstNum;
+                    if (firstNum < secondNum)
+                    {
+                        result = secondNum;
+                    }
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
