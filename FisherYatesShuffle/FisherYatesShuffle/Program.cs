6bae6defdae86d594d0a51ee0627a6004150daaa
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-10-09 23:05:23 -0500

    adds FisherYatesShuffle

---
+++ FisherYatesShuffle/FisherYatesShuffle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FisherYatesShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates the array that will be shuffled
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            //Shuffles the array of numbers
+            nums.FisherYatesShuffle();
+
+            //this prints out our numbers
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FisherYatesShuffle/FisherYatesShuffle/Shuffler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FisherYatesShuffle
+{
+    public static class Shuffler
+    {
+        private static Random randomNumber = new Random();
+
+        //call this from an array to shuffle it
+        public static void FisherYatesShuffle(this int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                //chooses the number that will be swaped with the current number
+                int j = randomNumber.Next((nums.Length - i));
+                //swaps the two numbers
+                int temp = nums[i];
+                nums[i] = nums[i+j];
+                nums[i+j] = temp;
+            }
+        }
+    }
+}
